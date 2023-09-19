92568481fa40a68e9fdd8d3f2114f5e1a56db2c1
Arantxa38 <141692165+Arantxa38@users.noreply.github.com>
2023-09-18 18:20:18 -0600

    Update Ejecutar.java

--- Actividad4/Ejecutar.java
+++ Actividad4/Ejecutar.java
@@ -1,13 +1,23 @@
 package package1;
 
-public class Ejecutar {
-	
-	public void IniciarPrograma(){
-		
-		MyQueues newQueue = new MyQueues();
-		MyStacks nStack = new MyStacks();
-		
-		nStack.push('a');
-		
-	}
+class Ejecutar {
+    public void iniciarPrograma() {
+        
+        MyStacks stack = new MyStacks(5);
+        stack.push('A');
+        stack.push('B');
+        stack.push('C');
+        System.out.println("Pop: " + stack.pop()); // Pop: C
+
+        MyQueues queue = new MyQueues(5);
+        queue.insert('X');
+        queue.insert('Y');
+        queue.insert('Z');
+        System.out.println("Eliminar: " + queue.delete()); // Delete: X
+
+        String str = "radar";
+        System.out.println("Frase invertida: " + stack.reverseString(str)); // Reversed String: radar
+        System.out.println(str + " es un palindromo?: " + stack.isPalindrome(str)); // Is Palindrome: true
+
+    }
 }
