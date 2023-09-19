a06c2abc91bd81a2ed2104c48877cba0a329ed48
Arantxa38 <141692165+Arantxa38@users.noreply.github.com>
2023-09-18 18:20:58 -0600

    Update MyQueues.java

--- Actividad4/MyQueues.java
+++ Actividad4/MyQueues.java
@@ -1,20 +1,40 @@
 package package1;
 
-public class MyQueues {
-	
-	public void insert (int data){
-		if(tail == MAX) {
-			System.out.println("Queue is full");
-		} else {
-			queue[tail] = data;/* inserts data*/
-		tail++; /* increases index*/}
-		}
-	
-	public char delete (int data){
-		if(tail == MAX) {
-			System.out.println("Queue is full");
-		} else {
-			queue[tail] = data;/* inserts data*/
-		tail++; /* increases index*/}}
-	}
-	}
+class MyQueues {
+    private char[] queue;
+    private int tail;
+    private int MAX;
+
+    public MyQueues(int size) {
+        MAX = size;
+        queue = new char[MAX];
+        tail = 0;
+    }
+
+    public void insert (char item){
+    	if(tail == MAX) {
+    		System.out.println("La cola esta llena");
+    	} else {
+    		queue[tail] = item;
+    		tail++; 
+    		}
+    	}
+
+    public char delete() {
+        if (tail > 0) {
+            char deletedItem = queue[0];
+            for (int i = 0; i < tail - 1; i++) {
+                queue[i] = queue[i + 1];
+            }
+            tail--;
+            return deletedItem;
+        } else {
+            System.out.println("La cola esta vacia");
+            return '#';
+        }
+    }
+
+    public boolean isEmpty() {
+        return (tail == 0);
+    }
+}
