fc04b80d02915b3f94e7203dd3b42b4b32dfaff2
Arantxa38 <141692165+Arantxa38@users.noreply.github.com>
2023-09-18 18:20:43 -0600

    Update MyStacks.java

--- Actividad4/MyStacks.java
+++ Actividad4/MyStacks.java
@@ -2,35 +2,54 @@ package package1;
 
 public class MyStacks {
 	
-	int[] stack;
-	int top = 0;
-	int MAX;
+	    private char[] stack;
+	    private int top;
+	    private int MAX;
+
+	    public MyStacks(int size) {
+	        MAX = size;
+	        stack = new char[MAX];
+	        top = -1;
+	    }
 	
-	public myStacks (int size) {
-		this.top = 0;
-		this.stack = new int [size];
-		
-	}
+	    public void push(char item) {
+	    	 if (top < MAX - 1) {
+	             stack[++top] = item;
+	         } else {
+	             System.out.println("El stack esta lleno");
+	         }
+	     }
 	
-	//char[] stack = {};
-	char stack[] = new char [10];
-	stack = new char[MAX];
-	//MAX = 10;
-	//top = ;
+	    public char pop(){
+	    	 if (top >= 0) {
+	             return stack[top--];
+	         } else {
+	             System.out.println("El stack esta vacio");
+	             return '#'; 
+	         }
+	     }
 	
-	public char pop(){  //elimina el ultimo valor
-		
-		if(top == 0){
-		System.out.println("Stack is empty");
-		return ();
-		
-		} else {
-			top ; /* decreases top */
-			return(); /*returns eliminated element*/
-			}
-	}
+	    public boolean isEmpty() {
+	        return (top == -1);
+	    }
+	    
+	    public String reverseString(String input) {
+	        int len = input.length();
+	        StringBuilder reversed = new StringBuilder(len);
 	
-	public char push(){
-		
-	}
+	        for (int i = 0; i < len; i++) {
+	            push(input.charAt(i));
+	        }
+	
+	        while (!isEmpty()) {
+	            reversed.append(pop());
+	        }
+	
+	        return reversed.toString();
+	    }
+	
+	    public boolean isPalindrome(String input) {
+	        String reversed = reverseString(input);
+	        return input.equals(reversed);
+	    }
 }
